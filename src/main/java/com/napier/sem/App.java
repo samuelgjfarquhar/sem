3aba5fb15f9469262ca2d4abd4792c9a840a1f9d
samuelgjfarquhar <40397363@live.napier.ac.uk>
2020-01-27 17:18:13 +0000

    fuck off#

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,9 +1,27 @@
 package com.napier.sem;
 
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
 public class App
 {
     public static void main(String[] args)
     {
         System.out.println("Group 17 Project");
     }
+
+}
+
+public class HelloWorldTest {
+
+    @Test
+    public void testNothing() {
+    }
+
+    @Test
+    public void testWillAlwaysFail() {
+        fail("An error message");
+    }
+
 }
