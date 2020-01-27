c000f5e6670fa79ec4c4a8c160d28eba32f83ed4
samuelgjfarquhar <40397363@live.napier.ac.uk>
2020-01-27 15:16:19 +0000

    gamer for life

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -4,6 +4,6 @@ public class App
 {
     public static void main(String[] args)
     {
-        System.out.println("wtf, so i can change this to anythign>?????");
+        System.out.println("Group 17 Project");
     }
 }
